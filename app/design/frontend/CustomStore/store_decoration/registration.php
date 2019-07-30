77ecfa2b6d161a5ad3f7b285ad16b58e8151edb4
prabhatkumarjena16 <prabhatkumarjena16@gmail.com>
2019-07-30 18:49:16 +0530

    Installation and folder structure setup for custom theme

---
+++ app/design/frontend/CustomStore/store_decoration/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/CustomStore/store_decoration',
+    __DIR__
+);
